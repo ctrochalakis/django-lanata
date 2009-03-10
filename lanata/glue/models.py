2af995e3f07164523a028bb0af8707673718a9fb
Christos Trochalakis <yatiohi@ideopolis.gr>
2009-03-10 17:37:41 -0400

    add fancy markdown post editor

--- lanata/glue/models.py
+++ lanata/glue/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.contrib import admin
+from django.db.models import get_model
 
-# Create your models here.
+admin.autodiscover()
+
+Post = get_model('blog','post')
+PostAdmin = type(admin.site._registry[Post])
+
+class NewPostAdmin(PostAdmin):
+    class Media:
+        js = ('js/wmd/wmd.js',)
+
+admin.site.unregister(Post)
+admin.site.register(Post, NewPostAdmin)
+
